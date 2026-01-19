1ce3b917a3828a39ab6920bb52c02d02be7b8aa3
adityarookie <sahishnuadityaa04@gmail.com>
2026-01-19 19:20:04 +0800

    fileupdate

---
+++ turtle1.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.Screen().bgcolor("Yellow")
+
+sc = turtle.Screen()
+sc.setup(400,300)
+
+turtle.title = "Welcome to the turtle screen"
+
+x = turtle.Turtle()
+
+for i in range(8):
+    x.forward(100)
+    x.right(90)
+
+
+

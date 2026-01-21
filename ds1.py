3c3d8559570326fb7cd53763266155dda017a806
adityarookie <sahishnuadityaa04@gmail.com>
2026-01-21 19:27:58 +0800

    operations on list

---
+++ ds1.py
@@ -0,0 +1,19 @@
+x = ["banana","apple","mango","watermelon"]
+
+print(x[2])
+
+print(x[1:5])
+
+x.reverse()
+print(x)
+
+x.sort()
+print(x)
+
+x.append("papaya")
+print(x)
+
+print(x[1:7])
+
+x.clear()
+print(x)

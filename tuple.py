ab58a8e00dc631fab55ed2afab7eb7faa8029834
adityarookie <sahishnuadityaa04@gmail.com>
2026-01-26 19:26:51 +0800

    operations on tuple

---
+++ tuple.py
@@ -0,0 +1,15 @@
+tuple_my = ()
+print(tuple_my)
+
+#print 4
+#print 3
+x = ("mouse", [8,4,6], (1,2,3))
+print(x[1][1])
+print(x[2][2])
+
+for i in x:
+    print("hello",i)
+
+print(x[2][0:3])
+#print(x[2:3]) this is wrong
+print(x[2])

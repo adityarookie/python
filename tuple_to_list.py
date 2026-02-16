1467f34f8876271b33983e035fcc31923aceae8c
adityarookie <sahishnuadityaa04@gmail.com>
2026-02-16 12:52:05 +0800

    tuple to list

---
+++ tuple_to_list.py
@@ -0,0 +1,9 @@
+
+Tuple = ("Sahishnu", "Adityaa",14,168, "Mathematics")
+
+# Step 2: Convert the tuple into a list
+List = list(Tuple)
+
+# Step 3: Print the result
+print("Tuple:", Tuple)
+print("List:", List)

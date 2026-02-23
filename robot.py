8e1d41037b2574363b6d318a01ce7f4b0cd35dfc
adityarookie <sahishnuadityaa04@gmail.com>
2026-02-23 18:19:47 +0800

    robot

--- robot.py
+++ robot.py
@@ -1,2 +1,14 @@
 class robot:
-    
+    def __init__(self,name):
+       self.name = "TOM"
+       
+    def introduce(self):
+        print("hello my name is",self.name)
+
+
+robot1 = robot("tom")
+robot2 = robot("jerry")
+
+robot1.introduce()
+robot2.introduce()
+

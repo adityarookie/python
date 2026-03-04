5eec216c3d48b4faa2e94514a7c3badff9bf8861
adityarookie <sahishnuadityaa04@gmail.com>
2026-03-04 19:17:12 +0800

    file handling advanced 2

---
+++ file3.py
@@ -0,0 +1,15 @@
+file = open("codingal.txt","r")
+print(file.read())
+file.close()
+
+file = open("codingal.txt","w")
+file.write(" I am a super expert at coding.Nothing else.")
+file.close()
+
+file = open("codingal.txt","w")
+file.write(" I am trying to be better")
+file.close()
+
+file = open("codingal.txt","r")
+print(file.read())
+file.close()

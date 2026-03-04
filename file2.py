5a900ad6bc551a069b26abd77eb33d4b5b400814
adityarookie <sahishnuadityaa04@gmail.com>
2026-03-04 18:59:08 +0800

    file handling advanced

---
+++ file2.py
@@ -0,0 +1,12 @@
+file = open("codingal.txt","r")
+print(file.read())
+file.close()
+
+file = open("codingal.txt","r")
+print(file.readline(9))
+file.close()
+
+file = open("codingal.txt","r")
+print(file.readlines())
+file.close()
+

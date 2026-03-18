ba6beae197e66d59b285000789a69fc5e2e8337c
adityarookie <sahishnuadityaa04@gmail.com>
2026-03-18 17:53:04 +0800

    turtle2

--- polygon.py
+++ polygon.py
@@ -1,4 +1,45 @@
+import turtle
 
+# Setup screen
+screen = turtle.Screen()
+screen.bgcolor("lightgreen")
+
+t = turtle.Turtle()
+t.speed(3)
+
+# --------- EQUILATERAL TRIANGLE ---------
+t.penup()
+t.goto(-100, 0)
+t.pendown()
+
+t.fillcolor("blue")
+t.begin_fill()
+
+for i in range(3):
+    t.forward(100)
+    t.left(120)
+
+t.end_fill()
+
+# --------- RECTANGLE ---------
+t.penup()
+t.goto(100, 0)
+t.pendown()
+
+t.fillcolor("orange")
+t.begin_fill()
+
+for i in range(2):
+    t.forward(150)
+    t.left(90)
+    t.forward(80)
+    t.left(90)
+
+t.end_fill()
+
+# Finish
+t.hideturtle()
+turtle.done()
 
 
 
